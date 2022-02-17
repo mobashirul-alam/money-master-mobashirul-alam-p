12cf24936ebe4ba43bbf8932b5eab889b9802283
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-02-17 22:26:05 +0600

    save section done and responsive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 
+// -------------------------Getting Input Value Function------------------------
 function getInputValue(inputId) {
     const inputString = document.getElementById(inputId).value;
     const inputValue = parseFloat(inputString);
@@ -13,6 +14,9 @@ function getInputValue(inputId) {
         return inputValue;
     }
 }
+
+// ------------------------Calculate button event handler------------------------
+
 document.getElementById('calculate-btn').addEventListener('click', function () {
     const incomeInput = getInputValue('income-input');
 
@@ -26,11 +30,34 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
         document.getElementById('total-expense-value').innerText = totalExpenses;
 
         // Set current balance 
-        const currentBalance = parseFloat(incomeInput) - totalExpenses;
-        document.getElementById('balance-current').innerText = currentBalance;
+        const balance = parseFloat(incomeInput) - totalExpenses;
+        let currentBalance = document.getElementById('balance-current').innerText = balance;
     }
+})
 
+// --------------------------Save button event handler--------------------------
 
+document.getElementById('save-button').addEventListener('click', function () {
+    // Saving Amount calculate.
+    const saveInput = getInputValue('save-input');
+    const savingAmount = getInputValue('income-input') / 100 * saveInput;
 
+    let currentBalance = parseFloat(document.getElementById('balance-current').innerText);
+
+    if (savingAmount < 0) {
+        alert('Invalid Input Given');
+        document.getElementById('save-input').value = "";
+    }
+    else if (savingAmount > currentBalance) {
+        alert("Can't save more than current balance.");
+        document.getElementById('saving-amount').innerText = "Fullfill your need first";
+        document.getElementById('save-input').value = "";
+    }
+    else {
+        // Remaining Balance calculate and Set
+        const balanceRemaining = currentBalance - savingAmount;
+        document.getElementById('saving-amount').innerText = savingAmount;
+        document.getElementById('balance-remaining').innerText = balanceRemaining;
+    }
 
 })
