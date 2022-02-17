5bfdd523cf4c0e4fada19d2534727952791be15c
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-02-17 18:22:47 +0600

    over expense handled

--- js/app.js
+++ js/app.js
@@ -1,19 +1,27 @@
 
-
+function getInputValue(inputId) {
+    const inputString = document.getElementById(inputId).value;
+    const inputValue = parseFloat(inputString);
+    return inputValue;
+}
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    const foodInput = document.getElementById('food-input').value;
-    const rentInput = document.getElementById('rent-input').value;
-    const clothesInput = document.getElementById('clothes-input').value;
-    const incomeInput = document.getElementById('income-input').value;
+    const incomeInput = getInputValue('income-input');
 
-    const totalExpenses = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothesInput);
+    const totalExpenses = getInputValue('food-input') + getInputValue('rent-input') + getInputValue('clothes-input');
 
-    // Set total expense value 
-    document.getElementById('total-expense-value').innerText = totalExpenses;
+    if (totalExpenses > incomeInput) {
+        alert('You can not spend more than you have.');
+        document.getElementById('total-expense-value').innerText = "Spend Carefully";
+    } else {
+        // Set total expense value 
+        document.getElementById('total-expense-value').innerText = totalExpenses;
 
-    // Set current balance 
-    const currentBalance = parseFloat(incomeInput) - totalExpenses;
-    document.getElementById('balance-current').innerText = currentBalance;
-    console.log(totalExpenses);
+        // Set current balance 
+        const currentBalance = parseFloat(incomeInput) - totalExpenses;
+        document.getElementById('balance-current').innerText = currentBalance;
+    }
+
+
+
 
 })
